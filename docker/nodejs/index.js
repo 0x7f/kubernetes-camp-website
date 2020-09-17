9661eacdf644061084224bedce773479325c4fad
Maximilian Haupt <mail@maximilianhaupt.com>
2020-09-17 09:06:05 +0200

    Improve nodejs example

--- docker/nodejs/index.js
+++ docker/nodejs/index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const getenv = require('getenv')
+const morgan = require('morgan')
 const redis = require('redis')
 
 const app = express()
 const port = getenv.int('PORT', 3000)
 const url = getenv('REDIS_URL', 'redis://127.0.0.1:6379')
 
+const start = Date.now()
+
 const client = redis.createClient({ url })
 client.on("error", err => console.error(err))
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/ready', (req, res) => {
+  const uptime = (Date.now() - start) / 1000
+  res.json({ uptime })
+})
+
+app.get('/healthy', (req, res) => {
+  client.get('somekey', (err, value) => {
+    if (err) {
+      res.sendStatus(500)
+      return
+    }
+    res.json({})
+  })
 })
 
 app.get('/get_value', (req, res) => {
